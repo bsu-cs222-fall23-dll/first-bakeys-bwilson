f3b0cfba21ba58db79fd1ba96489f48d61f322d0
Ben Keys <bkeys818@gmail.com>
2023-09-20 17:05:15 -0400

    ✅ Update ReversionParse to pass parseTest

--- src/main/java/WikipediaRevisionHistory/Revision.java
+++ src/main/java/WikipediaRevisionHistory/Revision.java
@@ -1,5 +1,12 @@
 package WikipediaRevisionHistory;
 
+import java.util.Map;
+
 public class Revision {
     public String timestamp, user;
+
+    public Revision(Map<String, String> revision) {
+        this.timestamp = revision.get("timestamp");
+        this.user = revision.get("user");
+    }
 }
--- src/main/java/WikipediaRevisionHistory/RevisionParser.java
+++ src/main/java/WikipediaRevisionHistory/RevisionParser.java
@@ -1,12 +1,23 @@
 package WikipediaRevisionHistory;
 
+import com.jayway.jsonpath.JsonPath;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class RevisionParser {
 
     public List<Revision> parse(InputStream dataStream) throws IOException {
-        return null;
+        List<Revision> list = new ArrayList<>();
+        List<List<Map<String,String>>> pages = JsonPath.read(dataStream, "$.query.pages.*.revisions");
+        pages.forEach(revisions -> {
+            revisions.forEach(revision -> {
+                list.add(new Revision(revision));
+            });
+        });
+        return list;
     }
 }
