0fb3d6c329f2d35c28c70429cee8f001942530fe
Ben Keys <bkeys818@gmail.com>
2023-09-20 17:27:32 -0400

    🧪 Create toStringTest in RevisionTest

---
+++ src/test/java/WikipediaRevisionHistory/RevisionTest.java
@@ -0,0 +1,18 @@
+package WikipediaRevisionHistory;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RevisionTest {
+    @Test
+    public void toStringTest() {
+        Map<String, String> map = new HashMap<>();
+        map.put("user", "Ken Gallager");
+        map.put("timestamp", "2022-05-25T17:55:11Z");
+        Revision revision = new Revision(map);
+        assertEquals("2022-05-25T17:55:11Z Ken Gallager\n", revision.toString());
+    }
+}
