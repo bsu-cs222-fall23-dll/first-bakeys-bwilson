4ca931096d263bc87e09fbaa9487a1d507e17684
Ben Keys <bkeys818@gmail.com>
2023-09-20 17:08:07 -0400

    ✨ Create WikipediaConnector

---
+++ src/main/java/WikipediaRevisionHistory/WikipediaConnector.java
@@ -0,0 +1,41 @@
+package WikipediaRevisionHistory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
+public class WikipediaConnector {
+    private final String title;
+
+    public WikipediaConnector(String title) {
+        this.title = title;
+    }
+
+    private URLConnection connectToWikipedia() throws IOException {
+        String urlString = "https://en.wikipedia.org/w/api.php?"
+                + "action=query"
+                + "&format=json"
+                + "&prop=revisions"
+                + "&rvprop=timestamp|user"
+                + "&rvlimit=4"
+                + "&redirects"
+                + "&titles=" + URLEncoder.encode(this.title, Charset.defaultCharset());
+        try {
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            connection.setRequestProperty("User-Agent", "CS222FirstProject/0.1 (bakeys@bsu.edu)");
+            connection.connect();
+            return connection;
+        } catch (MalformedURLException malformedURLException) {
+            throw new RuntimeException(malformedURLException);
+        }
+    }
+
+    private InputStream getDataFrom(URLConnection connection) throws IOException {
+        return connection.getInputStream();
+    }
+}
