eee98f8346dbb29551a908a8d262a5f74a067e3c
Ben Keys <bkeys818@gmail.com>
2023-09-22 15:35:21 -0400

    ✅ Update getRevisions tests in WikipediaParserTest

--- src/test/java/WikipediaRevisionHistory/WikipediaParserTest.java
+++ src/test/java/WikipediaRevisionHistory/WikipediaParserTest.java
@@ -1,5 +1,6 @@
 package WikipediaRevisionHistory;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
@@ -8,19 +9,32 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WikipediaParserTest {
-    InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+    InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("simple-test-data.json");
 
     @Test
     public void testInitialParse() {
         assertDoesNotThrow(() -> new WikipediaParser(testDataStream));
     }
 
-    @Test
-    public void testGetRevisions() {
-        WikipediaParser wikipediaParser = new WikipediaParser(testDataStream);
-        List<Revision> revisions = wikipediaParser.getRevisions();
-        assertEquals(revisions.size(), 4);
-        assertEquals(revisions.get(0).user, "Ken Gallager");
-        assertEquals(revisions.get(0).timestamp, "2022-05-25T17:55:11Z");
+    @Nested class getRevisions {
+        @Test
+        public void testParsing() {
+            List<Revision> revisions =  new WikipediaParser(testDataStream).getRevisions();
+            assertEquals(revisions.size(), 13);
+            assertEquals(revisions.get(0).user, "Ken Gallager");
+            assertEquals(revisions.get(0).timestamp, "2022-05-25T17:55:11Z");
+        }
+
+        @Test
+        public void testSorting() {
+            List<Revision> revisions =  new WikipediaParser(testDataStream).getRevisions();
+            String lastTimestamp = revisions.get(0).timestamp;
+            for (int i = 1; i < revisions.size(); i++) {
+                String timestamp = revisions.get(i).timestamp;
+                int diff = lastTimestamp.compareTo(timestamp);
+                assertTrue(diff >= 0, "Revisions not sorted reverse-chronological order");
+                lastTimestamp = timestamp;
+            }
+        }
     }
 }
