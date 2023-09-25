b19f0bc19081fb4a61e219760b7911180f1cba8f
Ben-jaminWilson <122378697+Ben-jaminWilson@users.noreply.github.com>
2023-09-25 18:06:47 -0400

    ✨ Create View

---
+++ src/main/java/WikipediaRevisionHistory/View.java
@@ -0,0 +1,26 @@
+package WikipediaRevisionHistory;
+
+import WikipediaRevisionHistory.Redirect;
+
+import java.util.List;
+
+public class View {
+    static void requestTitle(){
+        System.out.println("enter a Wikipedia title: ");
+
+    }
+    static void showNoInputWarning(){
+        System.out.println("I need an input stupid, provide Wiki article name");
+    }
+    static void showRevisions(){
+
+    }
+    static void showRedirectMessage(Redirect redirect){
+        System.out.println("Redirected to: " + redirect.to + "." );
+    }
+    static void showRevision(List<Revision> revisions){
+        revisions.forEach(revision -> {
+            System.out.println(revision.toString());
+        });
+    }
+}
