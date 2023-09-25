3c8623b567253863cc11ec26b28997b755b8aac7
Ben Keys <bkeys818@gmail.com>
2023-09-25 17:11:13 -0400

    🐛 Fix ControllerTest

--- src/test/java/WikipediaRevisionHistory/ControllerTest.java
+++ src/test/java/WikipediaRevisionHistory/ControllerTest.java
@@ -1,6 +1,10 @@
 package WikipediaRevisionHistory;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -10,14 +14,20 @@ public class ControllerTest {
     public void testGetArticleTitle() {
         Controller controller = new Controller();
         String title = "Arkansas Highway 327";
-        System.out.println(title);
+        setInput(title);
         assertEquals(title, controller.getTitle());
     }
 
     @Test
     public void testNoNoArticleTitleGiven() {
         Controller controller = new Controller();
-        System.out.println();
+        setInput("");
         assertThrows(NoInputException.class, controller::getTitle);
+    }
+
+    private void setInput(String text) {
+        text += "\n";
+        ByteArrayInputStream input = new ByteArrayInputStream(text.getBytes());
+        System.setIn(input);
     }
 }
