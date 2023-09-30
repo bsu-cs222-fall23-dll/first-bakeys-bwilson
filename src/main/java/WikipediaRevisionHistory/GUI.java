89899d4978668d3522079b03ac0effa0810e4930
Ben-jaminWilson <122378697+Ben-jaminWilson@users.noreply.github.com>
2023-09-30 12:38:24 -0400

    ✨ Create GUI

---
+++ src/main/java/WikipediaRevisionHistory/GUI.java
@@ -0,0 +1,21 @@
+package WikipediaRevisionHistory;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("hello world");
+        Scene scene = new Scene(label);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
