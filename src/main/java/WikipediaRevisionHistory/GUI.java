c5cd39e2caee4b688ba8f25880cd36b5926cd315
Ben Keys <bkeys818@gmail.com>
2023-10-02 17:56:09 -0400

    🏗️ Create view with scene builder

--- src/main/java/WikipediaRevisionHistory/GUI.java
+++ src/main/java/WikipediaRevisionHistory/GUI.java
@@ -1,9 +1,13 @@
 package WikipediaRevisionHistory;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class GUI extends Application {
 
@@ -12,10 +16,14 @@ public class GUI extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("hello world");
-        Scene scene = new Scene(label);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        URL url = getClass().getResource("/GUI.fxml");
+        if (url == null) throw new RuntimeException("Couldn't find GUI.fxml");
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);
+
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
     }
 }
