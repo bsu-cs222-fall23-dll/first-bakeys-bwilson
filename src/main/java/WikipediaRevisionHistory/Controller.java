5a96ba846b53fd0d32e85c413c4c09293361195b
Ben-jaminWilson <122378697+Ben-jaminWilson@users.noreply.github.com>
2023-10-02 22:32:48 -0400

    ✨ Updated Controller with search function

--- src/main/java/WikipediaRevisionHistory/Controller.java
+++ src/main/java/WikipediaRevisionHistory/Controller.java
@@ -1,16 +1,59 @@
 package WikipediaRevisionHistory;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.TextFlow;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+
+
+import javax.swing.text.html.ListView;
+import java.io.InputStream;
+import java.net.SocketTimeoutException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Controller {
+@FXML
+    private TextField userInput;
+@FXML
+    private ListView revisionList;
+@FXML
+    private TextFlow resultTextFlow;
+@FXML
+    public void search(ActionEvent actionEvent) throws SocketTimeoutException, NoArticleException {
+    resultTextFlow.getChildren().clear();
+    String title = userInput.getText();
+    WikipediaConnector wikiConnector = new WikipediaConnector(title);
+    InputStream data = wikiConnector.getData();
+    WikipediaParser parser = new WikipediaParser(data);
 
+    List<Revision> revisions = parser.getRevisions();
 
-    public String getTitle() throws NoInputException {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        if (input.isBlank())
-            throw new NoInputException();
-        scanner.close();
-        return input;
+    for (Revision revision : revisions) {
+        Text revisionText = new Text(revision.toString() + "\n");
+        resultTextFlow.getChildren().add(revisionText);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public String getTitle () throws NoInputException {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.isBlank())
+                throw new NoInputException();
+            scanner.close();
+            return input;
+        }
+    }
