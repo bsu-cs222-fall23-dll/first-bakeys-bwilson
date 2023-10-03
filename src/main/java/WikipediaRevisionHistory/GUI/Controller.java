3736470242bd286d49af761c560775b9ed5c9851
Ben Keys <bkeys818@gmail.com>
2023-10-03 13:05:47 -0400

    🚑️ Fix errors from merge

--- src/main/java/WikipediaRevisionHistory/GUI/Controller.java
+++ src/main/java/WikipediaRevisionHistory/GUI/Controller.java
@@ -29,7 +29,7 @@ public class Controller {
 
         }
         WikipediaConnector wikiConnector = new WikipediaConnector(title);
-        InputStream data = wikiConnector.getData();
+        String data = wikiConnector.getData();
         WikipediaParser parser = new WikipediaParser(data);
 
         List<Revision> revisions = parser.getRevisions();
@@ -38,20 +38,16 @@ public class Controller {
             Text revisionText = new Text(revision.toString() + "\n");
             resultTextFlow.getChildren().add(revisionText);
         }
-        List<Redirect> redirects = parser.getRedirects();
-        if (redirects != null && !redirects.isEmpty()) {
-            String RedirectGUI = View.showRedirectGUI(redirects.get(redirects.size() - 1));
-            Text TextRedirectGUI = new Text(RedirectGUI);
-
+        String lastRedirectDestination = parser.getLastRedirectDestination();
+        if (lastRedirectDestination != null) {
+            Text TextRedirectGUI = new Text("Redirected to: " + lastRedirectDestination);
             resultTextFlow.getChildren().add(TextRedirectGUI);
         }
 
 
     } catch (NoArticleException exception){
         Text NoArticleExceptionText = new Text(View.showNoArticleWarningGUI());
-
         resultTextFlow.getChildren().add(NoArticleExceptionText);
-
     }
 }
 }
--- src/main/java/WikipediaRevisionHistory/GUI/View.java
+++ src/main/java/WikipediaRevisionHistory/GUI/View.java
@@ -1,12 +1,6 @@
 package WikipediaRevisionHistory.GUI;
 
-import WikipediaRevisionHistory.model.Redirect;
-
 public class View {
-    static String showRedirectGUI(Redirect redirect){
-        return "Redirected to: " + redirect.to + ".";
-
-    }
     static String showNoArticleWarningGUI(){
         return("That doesn't exists... anywhere on the internet.. dummy");
     }
